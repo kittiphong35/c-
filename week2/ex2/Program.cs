c873864d78be40b89b08fd1a92d9281c275b4166
kittiphong35 <142704924+kittiphong35@users.noreply.github.com>
2023-08-20 18:48:14 +0700

    first commit

---
+++ week2/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      int input1 = 8;
+      int input2 = 5;
+      int total = input1 - input2;
+
+      Console.WriteLine("addition=");
+      Console.WriteLine(total);
+      
+       }
+  }
+}
+
---
+++ week2/ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      int input1 = 30;
+      int input2 = 6;
+      int total = input1 / input2;
+
+      Console.WriteLine("addition=");
+      Console.WriteLine(total);
+      
+       }
+  }
+}
+
+
+

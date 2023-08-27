adca837f00c8b2555a8bd8bf2033c34a3e522aa1
kittiphong35 <142704924+kittiphong35@users.noreply.github.com>
2023-08-27 17:31:15 +0700

    Add files via upload

---
+++ week3/user input/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string fname = "";
+double height = 0;
+double weight = 0.0d;
+double BMI =0.0d;
+
+Console.Write("input first name : ");                 //display  ชื่อ
+fname = Console.ReadLine();                          //input keyboard
+
+Console.Write("input height : ");               //display ส่วนสูง
+height = double.Parse(Console.ReadLine());          //input keyboard and convertheight 
+
+Console.Write("input weight : ");               //display น้ำหนัก
+weight = double.Parse(Console.ReadLine());          //input keyboard and convertheight 
+
+BMI = height/weight;
+
+BMI = Math.Pow(BMI,2);
+
+Console.WriteLine("BMI is"+BMI);      
